db448e296db310572b582b05e490076a32c90b9b
ng.kasatkin <nikita.kasatkin44@gmail.com>
2020-07-03 15:42:46 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // CSS
 require('../css/general.css')
-// require('../css/owl.carousel.min.css')
 
 // JS
 window.$ = require('jquery')
@@ -9,4 +8,4 @@ require('./owl.carousel.min.js')
 require('../images/favicon.ico')
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.css'
+import 'font-awesome/css/font-awesome.css';
