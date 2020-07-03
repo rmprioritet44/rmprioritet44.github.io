429df9984744b0a16741acea25db38f543ad5397
ng.kasatkin <nikita.kasatkin44@gmail.com>
2020-07-03 16:51:33 +0300

    12

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 // CSS
 require('../css/general.css')
+// require('../css/slider.css')
+
+import 'slick-carousel'
+
 
 // JS
 window.$ = require('jquery')
@@ -8,4 +12,6 @@ require('./owl.carousel.min.js')
 require('../images/favicon.ico')
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'font-awesome/css/font-awesome.css';
+// import 'slick-carousel/slick/slick'
--- src/js/script.js
+++ src/js/script.js
@@ -3,34 +3,13 @@ $('.navbar-nav .nav-link').click(function(){
     $(this).addClass('active');
 });
 
-$(document).ready(function(){
-    // $('.owl-carousel').owlCarousel({
-    //     navigation: true,
-    //     navText: ["<img src='/images/leftBtn.png'>", "<img src='images/rightBtn.png'>"],
-    //     autoWidth: true,
-    //     items: 3,
-    //     margin: 45,
-    //     nav: true,
-    //     loop: true,
-    //     autoplay: true,
-    //     autoplayTimeout: 5000,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         0:{
-    //             items:1,
-    //             nav:true
-    //         },
-    //         600:{
-    //             items:2,
-    //             nav:true
-    //         },
-    //         1000:{
-    //             items:3,
-    //             nav:true
-    //         }
-    //     }
-    // });
+import $ from "jquery";
+import "slick-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+
+$(document).ready(function(){
     $('.email-form-btn').click(function(){
         $('#myModal').modal('show')
     });
@@ -42,21 +21,6 @@ $(document).ready(function(){
     $('.vk-btn').click(function(){
         window.open('https://vk.com/rm_prioritet44', '_blank')
     });
-
-    // $('[name="phone"]').keyup(function(e)
-    // {
-    //     if(e.keyCode > 36 && e.keyCode < 41)
-    //     {
-    //         return true;
-    //     }
-    //     if ((e.keyCode > 47 && e.keyCode <58) || (e.keyCode < 106 && e.keyCode > 95))
-    //     {
-    //         // this.value = this.value.replace(/(\d{3})(\d{3})(\d{2})(\d{2})$/);
-    //         this.value = this.value.replace(/()(\d{3})\-?/g,'$1-');
-    //         return true;
-    //     }
-    //     this.value = this.value.replace(/[^\-0-9]/g,'');
-    // });
 
     function phoneMask() {
         var num = $(this).val().replace(/\D/g,'');
@@ -103,103 +67,3 @@ $(document).ready(function(){
     }
     $('[name="phone"]').keyup(phoneMask);
 });
-
-// $(document).ready(function(){
-//     $('.my-own-class').slick({
-//         variableWidth: true,
-//         slidesToShow: 3
-//     })
-// });
-
-// $(document).ready(function(){
-//     $('.my-own-class').slick({
-//         // variableWidth: true,
-//         dots: true,
-//         infinite: false,
-//         speed: 300,
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         responsive: [
-//             {
-//                 breakpoint: 2000,
-//                 settings: {
-//                     slidesToShow: 3,
-//                     slidesToScroll: 3,
-//                     infinite: true,
-//                     dots: true
-//                 }
-//             },
-//             {
-//                 breakpoint: 1000,
-//                 settings: {
-//                     slidesToShow: 2,
-//                     slidesToScroll: 2
-//                 }
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1
-//                 }
-//             }
-//             // You can unslick at a given breakpoint now by adding:
-//             // settings: "unslick"
-//             // instead of a settings object
-//         ]
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('.my-own-class').slick({
-//         variableWidth: true,
-//         // centerMode: true,
-//         // centerPadding: '120px',
-//         slidesToShow: 3,
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     arrows: false,
-//                     centerMode: true,
-//                     centerPadding: '40px',
-//                     slidesToShow: 3
-//                 }
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     arrows: false,
-//                     centerMode: true,
-//                     centerPadding: '40px',
-//                     slidesToShow: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
-
-// function setSlideVisibility() {
-//     //Find the visible slides i.e. where aria-hidden="false"
-//     var visibleSlides = $('.slick-slide[aria-hidden="false"]');
-//     //Make sure all of the visible slides have an opacity of 1
-//     $(visibleSlides).each(function() {
-//         $(this).css('opacity', 1);
-//     });
-//     //Set the opacity of the first and last partial slides.
-//     $(visibleSlides).first().prev().css('opacity', 0);
-//     $(visibleSlides).last().next().css('opacity', 0);
-// }
-//
-// $(setSlideVisibility());
-//
-// $('.my-own-class').on('beforeChange', function() {
-//     $('.slick-slide').each(function() {
-//         $(this).css('opacity', 1);
-//     });
-// });
-//
-// $('.my-own-class').on('afterChange', function() {
-//     setSlideVisibility();
-// });
-
