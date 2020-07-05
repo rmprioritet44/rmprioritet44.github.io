3119d7b2d9719eb2c0a4b6b804ceec3a02828475
ng.kasatkin <nikita.kasatkin44@gmail.com>
2020-07-05 12:33:29 +0300

    шрифты

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -45,14 +45,14 @@ module.exports = {
         ]
       },
       {
-        test: /\.(woff|woff2|ttf)$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
-            // options: {
-            //   name: '[name].[ext]',
-            //   outputPath: 'fonts/'
-            // }
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/fonts'
+            }
           }
         ]
       },
